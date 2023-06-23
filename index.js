1b1253a26cb30932c045fc5fe7c3bcb095b49a3e
leilaashur <leilahashur@gmail.com>
2023-06-23 09:16:38 +0300

    todo commiti

--- index.js
+++ index.js
@@ -1,32 +1,100 @@
-const addButton = document.getElementById("addButton");
-const deadlineInput = document.getElementById("deadlineInput");
-const taskList = document.getElementById("taskList");
-
-addButton.addEventListener("click", addTask);
-
-function addTask() {
-  const taskText = deadlineInput.value.trim();
-
-  if (taskText !== "") {
-    const li = document.createElement("li");
-    li.textContent = taskText;
-
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", function () {
+const userContainer = document.getElementById('Task');
+const successMessage = document.getElementById('Message');
+const addBtn = document.getElementById('add');
+const deleteBtn = document.getElementById('button2');
+const taskIdInput = document.getElementById('taskId');
+const getTodoById = async (userId) => {
+  try {
+    const response = await fetch(`https://dummyjson.com/todos/${userId}`);
+    const todo = await response.json();
+    return todo;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+const displayTodo = async (userId) => {
+  const todo = await getTodoById(userId);
+  if (todo) {
+    console.log(todo);
+    let li = document.createElement('li');
+    let userName = document.createElement('input');
+    let checkbox = document.createElement('input');
+    let deleteImg = document.createElement('img');
+    let gap = document.createElement('span');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    userName.value = todo.todo;
+    userName.style.width = '500px';
+    userName.style.height = '20px';
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        userName.style.textDecoration = 'line-through';
+      } else {
+        userName.style.textDecoration = 'none';
+      }
+    });
+    deleteImg.src = 'bin-removebg-preview.png';
+    deleteImg.style.width = '20px';
+    deleteImg.style.height = '20px';
+    deleteImg.addEventListener('click', () => {
+      deleteTodoById(userId);
       li.remove();
     });
-
-    const completeButton = document.createElement("button");
-    completeButton.textContent = "Complete";
-    completeButton.addEventListener("click", function () {
-      li.classList.toggle("completed");
-    });
-
-    li.appendChild(deleteButton);
-    li.appendChild(completeButton);
-    taskList.appendChild(li);
-
-    deadlineInput.value = "";
+    gap.style.marginRight = '3px';
+    li.appendChild(checkbox);
+    li.appendChild(userName);
+    li.appendChild(deleteImg);
+    li.appendChild(gap);
+    li.setAttribute('key', userId);
+    li.setAttribute('class', 'task');
+    userContainer.appendChild(li);
+    successMessage.textContent = 'Task Complete.';
+  } else {
+    console.log(`Todo with user ID ${userId} not found.`);
   }
-}
+};
+const deleteTodoById = async (userId) => {
+  try {
+    const response = await fetch(`https://dummyjson.com/todos/${userId}`, {
+      method: 'DELETE'
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete todo');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const deleteTasks = () => {
+  const tasks = document.getElementsByClassName('task');
+  while (tasks.length > 0) {
+    tasks[0].remove();
+  }
+  successMessage.style.display = 'none';
+};
+addBtn.addEventListener('click', async () => {
+  const userId = parseInt(taskIdInput.value);
+  if (!isNaN(userId)) {
+    const todo = await getTodoById(userId);
+    if (todo) {
+      console.log(`Todo with user ID ${userId} Task Added.`);
+      successMessage.style.display = 'block';
+      successMessage.textContent = 'Task Added Successfuly.';
+      setTimeout(() => {
+        successMessage.style.display = 'none';
+      }, 1000);
+      displayTodo(userId);
+    } else {
+      console.log(`Todo with user ID ${userId} not found.`);
+    }
+  } else {
+    successMessage.style.display = 'block';
+    successMessage.textContent = 'Please enter a valid User ID.';
+  }
+});
+
+
+
+
+
